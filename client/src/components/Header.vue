d1d480d2f473a71f0b3fecc0e7274d4df1d43d87
fedorkarpenko <fedorkarpenko1984@gmail.com>
2022-05-17 17:33:11 +0300

    Инит для фронэндат

---
+++ client/src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+    <header class="header">
+
+    </header>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+            greeting: ''
+        }),
+
+        mounted() {
+            const hours = new Date().getHours()
+            console.log(hours)
+        }
+    }
+</script>
+
+<style scoped>
+
+.header {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    display: flex;
+    align-items: center;
+}
+
+</style>
